20a90b4dc2d003707343ced8195e21fb6dea6ee9
SantoshPanta <santoshpanta50@gmail.com>
2023-03-10 08:05:01 +0545

    added basic files

---
+++ npi.php
@@ -0,0 +1,37 @@
+<?php
+
+defined('ABSPATH') or die('No script kiddies please!!');
+/**
+ * Plugin Name:       Npi 
+ * Plugin URI:        https://santoshpanta.com/
+ * Description:       A contact form for npi.
+ * Version:           1.0.0
+ * Requires at least: 5.4
+ * Requires PHP:      7.4
+ * Author:            Santosh Panta
+ * Author URI:        https://santoshpanta.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       npi
+ * Domain Path:       /languages
+ */
+
+if(!class_exists('NPI')) {
+    
+    class NPI
+    {
+        
+        function __construct()
+        {
+            $this->hello();
+        }
+
+        function hello()
+        {
+            echo 'Hello World!';
+        }
+        
+    }
+
+    new NPI;
+}
